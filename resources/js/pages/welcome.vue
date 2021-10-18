dec0eaee86f5f952a2a4af38be7e3608682aaec5
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-10-19 02:18:21 +0900

    initial version

---
+++ resources/js/pages/welcome.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div class="top-right links">
+      <template v-if="authenticated">
+        <router-link :to="{ name: 'home' }">
+          {{ $t('home') }}
+        </router-link>
+      </template>
+      <template v-else>
+        <router-link :to="{ name: 'login' }">
+          {{ $t('login') }}
+        </router-link>
+        <router-link :to="{ name: 'register' }">
+          {{ $t('register') }}
+        </router-link>
+      </template>
+    </div>
+
+    <div class="text-center">
+      <div>
+        <img class="titleImage" src="/assets/image/LOGO_GFEX.png" width="300" height="300">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  layout: 'basic',
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  },
+
+  data: () => ({
+    title: window.config.appName
+  }),
+
+  computed: mapGetters({
+    authenticated: 'auth/check'
+  })
+}
+</script>
+
+<style scoped>
+.top-right {
+  position: absolute;
+  right: 10px;
+  top: 18px;
+}
+
+.title {
+  font-size: 85px;
+}
+</style>
