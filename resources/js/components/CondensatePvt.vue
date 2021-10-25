8a80f7d8aa32849e12304a6d9190bf6080814bce
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-10-26 07:13:20 +0900

    added plot feature

--- resources/js/components/CondensatePvt.vue
+++ resources/js/components/CondensatePvt.vue
@@ -14,17 +14,34 @@
       <div id="gasCondensate2"></div>
     </div>
 
+    <hr class="gf-line" v-show="bShowPlot == true">
     <div style="display:flex;margin-bottom:6px;text-align:center;min-height:400px" class="row" v-show="bShowPlot == true">
       <div class="col-3">
         <label class="typo__label gf-item">Axis X:</label>
-        <multiselect v-model="axisX" :options="options" placeholder="Select X axis."></multiselect>
+        <multiselect v-model="axisX" :options="options" track-by="name" label="name" :taggable="true" placeholder="Select X axis."></multiselect>
         <label class="typo__label gf-item">Axis Y:</label>
-        <multiselect v-model="axisY" :close-on-select="false" :options="options" :maxHeight="250" :multiple="true" :taggable="true" placeholder="Select multiple Y axis."></multiselect>
+        <multiselect v-model="axisY" :options="options" track-by="name" label="name" :close-on-select="false" :maxHeight="250" :multiple="true" :taggable="true" placeholder="Select multiple Y axis."></multiselect>
+        <label class="typo__label gf-item">Axis Y2:</label>
+        <multiselect v-model="axisY2" :options="options" track-by="name" label="name" :taggable="true" placeholder="Select Y2 axis."></multiselect>
       </div>
       <div class="col-2">
         <label class="btn btn-primary gf-button" style="margin-top:85px" v-on:click="onShow">Show</label>
       </div>
       <div id="plot" class="col-7">
+      </div>
+    </div>
+
+    <div id="plotModal" class="gf-modal">
+      <div class="gf-modal-content">
+        <div class="gf-modal-header">
+          <span class="gf-comment" style="margin-left:30px;color:white">Fastplan* platform</span>
+          <span class="gf-close" id="plot-gf-close">&times;</span>
+        </div>
+        <p class="gf-comment" style="margin-top:6px !important; margin-bottom:6px !important;"><{{projectName}}> Field Project</p>
+        <span style="font-size: 1.25rem">Please select all X, Y axes</span>
+        <div style="margin-bottom:16px;margin-top:16px">
+          <label class="btn btn-primary gf-button" v-on:click="onOK">OK</label>
+        </div>
       </div>
     </div>
 
@@ -54,9 +71,19 @@ export default {
       gasCondensate2: null,
       myGasCondensate : {},
       bShowPlot : false,
-      options: ["P (psia)", "Bo (rb/stb)", "Rs (scf/stb)", "Bg (rb/mscf)", "Rv (stb/mmscf)", "Oil Viscosity (cp)", "Gas Viscosity (cp)", "PV Inj (%)"],
+      options: [
+        { name: "P (psia)", column: 'A'}, 
+        { name: "Bo (rb/stb)", column: 'B'},
+        { name: "Rs (scf/stb)", column: 'C'}, 
+        { name: "Bg (rb/mscf)", column: 'D'},
+        { name: "Rv (stb/mmscf)", column: 'E'}, 
+        { name: "Oil Viscosity (cp)", column: 'F'}, 
+        { name: "Gas Viscosity (cp)", column: 'G'},
+        { name: "PV Inj (%)", column:'H'}
+      ],
       axisX: null,
       axisY: [],
+      axisY2: null,
       plot: null
     }
   },
@@ -71,8 +98,9 @@ export default {
   },
 
   methods: {
-    onShow: function(event) {
-      this.updatePlot();
+    onOK: function(event) {
+        var modal = document.getElementById("plotModal");
+        modal.style.display = "none";
     },
     onPlotPage: function(event) {
       if(this.bShowPlot == false)
@@ -107,25 +135,105 @@ export default {
 
       await store.dispatch('project/saveGasCondensate', this.myGasCondensate)
     },
-    updatePlot: function() {
+    onShow: function(event) {
+      
+      // ----------------------------------------------------------
+      // Validation
+      // ----------------------------------------------------------
+
+      if (this.axisX == null || this.axisY.length == 0) {
+        var modal = document.getElementById("plotModal");
+        modal.style.display = "block";
+        return;
+      }
+
+      // ----------------------------------------------------------
+      // Initialize variables
+      // ----------------------------------------------------------
+
+      var axisX = this.axisX.name
+      var columns = [
+              // ['P (psia)', 5114, 4000, 3000, 2000, 1000],
+              // ['Bo (rb/stb)', 5.094, 5.094, 1.66, 1.409, 1.219],
+              // ['Oil Viscosity (cp)', 0.062, 0.062, 0.4781, 0.7746, 1.0295],
+              // ['RS (scf/stb)', 7479.4, 7479.4, 1778.9, 1075.8, 596.4],
+              // ['Bg (rb/mscf)', 0.681, 0.681, 0.958, 1.376, 2.636],
+            ]
+      var axes = {}
+      var ylabel = null
+      var ylabel2 = null
+
+      // ----------------------------------------------------------
+      // Start HERE
+      // ----------------------------------------------------------
+      debugger
+      var numRows = this.gasCondensate2.options.data.length;
+      
+      // ----------------------------------------------------------------
+      // add x data
+      columns[0] = []
+      columns[0][0] = this.axisX.name
+      for (let index = 1; index <= numRows; index++) {
+        columns[0][index] = this.gasCondensate2.getValue(this.axisX.column + '' + index)        
+      }
+      
+      // ----------------------------------------------------------------
+      // add y data
+      for (let i = 0; i < this.axisY.length; i++) {
+
+        if (i == 0) {
+          ylabel = this.axisY[i].name
+        }
+
+        columns[i+1] = []
+        columns[i+1][0] = this.axisY[i].name
+        for (let j = 1; j <= numRows; j++) {
+          columns[i+1][j] = this.gasCondensate2.getValue(this.axisY[i].column + '' + j)
+        }
+      }
+
+      // ----------------------------------------------------------------
+      // add y2 data
+      if (this.axisY2 != null) {
+        columns[this.axisY.length+1] = []
+        columns[this.axisY.length+1][0] = this.axisY2.name
+        for (let j = 1; j <= numRows; j++) {
+          columns[this.axisY.length+1][j] = this.gasCondensate2.getValue(this.axisY2.column + '' + j)
+        }
+
+        axes[this.axisY2.name] = 'y2'
+        ylabel2 = this.axisY2.name
+      }
+
+      this.updatePlot(axisX, columns, axes, ylabel, ylabel2);
+    },
+    updatePlot: function(_axisX, _columns, _axes, _ylabel, _ylabel2) {
       this.plot = c3.generate({
           bindto: '#plot',
           data: {
-            columns: [
-              ['data1', 30, 200, 100, 400, 150, 250],
-              ['data2', 50, 20, 10, 40, 15, 25]
-            ],
-            axes: {
-              data2: 'y2' // ADD
-            },
-            types: {
-              'data1': 'line',
-              'data2': 'line'
-            }
+            x: _axisX,
+            columns: _columns,
+            axes: _axes,
           },
           axis: {
+            x: {
+              label: {
+                text: _axisX,
+                position: 'outer-middle'
+              }
+            },
+            y: {
+              label: {
+                text: _ylabel,
+                position: 'outer-middle'
+              }
+            },
             y2: {
-              show: true // ADD
+              show: _ylabel2 != null, // ADD
+              label: {
+                text: _ylabel2,
+                position: 'outer-middle'
+              }
             }
           }
       });
@@ -238,7 +346,31 @@ export default {
         ]
     });
     this.gasCondensate2.hideIndex();
+
+    mountPlotDialog();
   }
 
 }
+
+function mountPlotDialog() {
+
+  // Get the modal
+  var modal = document.getElementById("plotModal");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementById("plot-gf-close");
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }      
+}
+
 </script>
