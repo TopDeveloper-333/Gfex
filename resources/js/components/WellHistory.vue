2d4bc784a8375b02dc1edb81b883eacd368c16a8
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-10-28 01:48:37 +0900

    added dynamic items

--- resources/js/components/WellHistory.vue
+++ resources/js/components/WellHistory.vue
@@ -24,9 +24,24 @@
 
     <div style="display:flex;margin-bottom:6px;text-align:left" class="row">
       <p class="gf-item">Number of Wells in Network (Present & Futures)</p>
+      <input class="form-control gf-control" style="width:500px; margin-left:10px; margin-bottom:20px" type="number"
+          maxlength="20" v-model="numberOfWells" placeholder="Please input numbers of Wells">
     </div>
 
-    <div id="dynamicData">
+    <div v-for="entry in wellsNetwork" :key="entry.id"
+      style="display:flex;margin-bottom:6px;text-align:left" class="row">
+      <!-- {{entry.id}} : {{ entry.option }} : {{entry.data}} -->
+      <hr class="gf-line">
+
+      <p class="gf-item">Well-Test Data #{{entry.id}}</p>
+      <multiselect v-model="entry.option" :options="testWellDataOptions" @select="onChangedOption"
+        track-by="name" label="name" placeholder="Select option" :id="'option-' + entry.id"></multiselect>
+
+      <p class="gf-item" v-show="entry.option!=null&&entry.option.value==1">C & N Model #{{entry.id}}</p>
+      <p class="gf-item" v-show="entry.option!=null&&entry.option.value==2">Vertical Well Model #{{entry.id}}</p>
+      <p class="gf-item" v-show="entry.option!=null&&entry.option.value==3">Horizontal Well Model #{{entry.id}}</p>
+
+      <div :id="'networkSheet-' + entry.id"></div>
     </div>
 
   </div>
@@ -69,7 +84,14 @@ export default {
       dualPorosityOptions: [
         {name: "Yes", value: 1},
         {name: "No", value: 0}
-      ]
+      ],
+      numberOfWells: 0,
+      wellsNetwork: [],
+      testWellDataOptions: [
+        { name: "C & N Method", value : 1 },
+        { name: "Vertical Model", value: 2 },
+        { name: "Horizontal Model", value: 3 }
+      ],
     }
   },
 
@@ -79,8 +101,172 @@ export default {
     })
   },
 
-  methods: {
+  watch: {
+    numberOfWells: function(val, oldVal) {
+      if (val < 0)
+        val = 0;
+      if (oldVal < 0)
+        oldVal = 0;
 
+      this.numberOfWells = val;
+
+      debugger
+      if (val > oldVal) {
+        // add new entry to wellsNetwork
+        for (let index = 0; index < (val - oldVal); index++) {
+          this.wellsNetwork.push(
+            {
+              id: this.wellsNetwork.length, 
+              option:null, 
+              sheet: null
+            }
+          )
+        }
+      }
+      else if (val < oldVal) {
+        // remove entry from wellsNetwork
+        for (let index = 0; index < (oldVal - val); index++) {
+          this.wellsNetwork.pop()
+        }
+      }
+    },
+  },
+
+  methods: {
+    onChangedOption: function(selectedOption, id) {
+
+      var index = id.replace('option-','');
+      let divId = 'networkSheet-' + index;
+      var columns = []
+
+      if (index >= this.wellsNetwork.length) {
+        alert('Selected Id is out of number.')
+        return;
+      }
+
+      if (selectedOption.value == 1) {
+        columns = [
+            {
+                type: 'numeric',
+                title:'C (Mscf/day/psia2)',
+                width: 190,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'N',
+                width: 120,
+                decimal:','
+            },
+        ]
+      }
+      else if (selectedOption.value == 2) {
+        columns = [
+            {
+                type: 'numeric',
+                title:'k (md)',
+                width: 100,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Porosity',
+                width: 100,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Net Pay (ft)',
+                width: 140,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Drainage Area (acres)',
+                width: 220,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Wellbore ID (in)',
+                width: 160,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Skin',
+                width: 90,
+                decimal:','
+            },
+        ]
+      }
+      else if (selectedOption.value == 3) {
+        // Horizontoal Well Model
+        columns = [
+            {
+                type: 'numeric',
+                title:'k (md)',
+                width: 90,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Porosity',
+                width: 100,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Net Pay (ft)',
+                width: 120,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Drainage Area (acres)',
+                width: 220,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Wellbore ID (in)',
+                width: 160,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Skin',
+                width: 90,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Well Length',
+                width: 120,
+                decimal:','
+            },
+            {
+                type: 'numeric',
+                title:'Kv/Kh',
+                width: 90,
+                decimal:','
+            },
+        ]
+      }
+
+      document.getElementById(divId).innerHTML = '';
+      this.wellsNetwork[index].sheet = jspreadsheet(document.getElementById(divId), {
+          data:[[]],
+          allowInsertRow:false,
+          allowManualInsertRow:false,
+          allowInsertColumn:false,
+          allowManualInsertColumn:false,
+          allowDeleteRow:false,
+          allowDeleteColumn:false,
+          columns: columns
+      });
+      this.wellsNetwork[index].sheet.hideIndex();
+    }
   },
 
   mounted() {
@@ -116,7 +302,6 @@ export default {
     this.reservoirDataHSheet.hideIndex();
 
     // dualPorosity data
-    debugger
     var dualPorosityData = [
       [1, 300, 0.00001, 15000]
     ];
