2b624c2e738c18e18cc57289e813ccb478080b0f
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2021-11-05 00:06:06 +0900

    fixed font for welcome screen

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -18,15 +18,15 @@
 
     <div class="text-center">
       <div>
-        <p style="font-size:2.5rem !important;font-weight:600;color:#4172c4">"FastPlan*"</p>
-        <p style="font-size:2rem !important;font-weight:400;color:#4172c4">DEVELOPMENT SCREENING / PLANNING<br/>
+        <p style="font-size:7rem !important;font-weight:600;color:#4172c4">FastPlan*</p>
+        <p style="font-size:3rem !important;font-weight:600;color:#4172c4">DEVELOPMENT SCREENING / PLANNING<br/>
         &<br/>
         FIELD MONITORING / SURVEILLANCE<br/>
         PLATFORM</p>
 
-        <img class="titleImage" src="/assets/image/LOGO_GFEX.png" width="200" height="200">
+        <img class="titleImage" src="/assets/image/LOGO_GFEX.png" width="450" height="450">
 
-        <p style="font-size:1rem !important;font-weight:300;color:#4172c4">GFeX, LLC<br/>
+        <p style="font-size:1.5rem !important;font-weight:600;color:#4172c4">GFeX, LLC<br/>
         73 Timberlake Dr<br/>
         Suga Land, Texas 77479<br/>
         USA<br/>
