29093771c7223201cfb545fcd5d7c0fbeb90eef2
@wahaha-rus <69617937+TopDeveloper-333@users.noreply.github.com>
2022-01-27 00:42:09 +0900

    added website installation guideline

---
+++ gfex-2022-init.sql
@@ -0,0 +1,2 @@
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `is_admin`, `role`, `from`, `to`, `is_revoke`) VALUES
+(1, 'Administrator', 'admin@gfex.com', NULL, '$2y$10$6STL69Oo3yCl2PUe8HYOG.axDuJ/ltdfL5fUBy2yX9bjoBQD3NtZq', NULL, '2021-11-02 01:55:51', '2021-11-18 13:55:59', 1, 0, NULL, NULL, 0);
